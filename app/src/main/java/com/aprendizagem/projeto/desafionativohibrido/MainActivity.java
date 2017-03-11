48e6a7808dc73e9ac208a976b537c1b966b12295
ManuJS <emanuelle.menali@hotmail.com>
2017-03-11 14:17:55 -0300

    parte da camera

--- app/src/main/java/com/aprendizagem/projeto/desafionativohibrido/MainActivity.java
+++ app/src/main/java/com/aprendizagem/projeto/desafionativohibrido/MainActivity.java
@@ -1,5 +1,7 @@
 package com.aprendizagem.projeto.desafionativohibrido;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
     }
 }
